095056beb8cdb8b02b28fb07c012e636a61973dc
christiezhong <49793113+christiezhong@users.noreply.github.com>
2020-04-02 12:34:52 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// let xBall = Math.floor(Math.random() * 300) + 50;
+// let yBall = 50;
+// let xSpeed = (5, 9);
+// let ySpeed = (-7, -2);
+
+// function setup() {
+//     createCanvas(windowWidth, windowHeight);
+  
+//   }
+//   //Background
+  
+//   function draw() {
